ca7bb092381709d355a68ea07a9888a96dca2465
Pranshu <pranshutijil23@gmail.com>
2025-07-06 22:28:25 +0530

    Fixed Render port Binding problem

--- app.py
+++ app.py
@@ -245,5 +245,8 @@ def download():
         download_name=filename
     )
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
